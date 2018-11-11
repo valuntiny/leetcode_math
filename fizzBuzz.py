bf59ba7aa7e09a842f0b39e54748f381f0c6dd58
valuntiny <459201296@qq.com>
2018-11-10 23:01:44 -0500

    using boolean type (not ...) and mod (%) and [... or ...] to solve this problem

---
+++ fizzBuzz.py
@@ -0,0 +1,15 @@
+'''
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+'''
+
+
+class Solution:
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1,n+1)]
